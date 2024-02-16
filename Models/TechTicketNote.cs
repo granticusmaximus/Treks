1fdf1db64702aace31ac5cbcf51fd477a3256b52
Grant Watson <grant@gwsapp.net>
2024-02-16 15:55:49 -0500

    Update code for displaying ticket notes in TicketDetails page

--- Models/TechTicketNote.cs
+++ Models/TechTicketNote.cs
@@ -9,7 +9,6 @@ namespace Treks.Models
     {
         public string TicketId { get; set; }
         public int TechNoteId { get; set; }
-
         public Ticket Ticket { get; set; }
         public TechNote TechNote { get; set; }
     }
