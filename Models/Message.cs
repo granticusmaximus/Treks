f4edee4b3766dbedea806c5098a63e9955f2855b
Grant Watson <grant@gwsapp.net>
2024-03-06 16:56:26 -0500

    Add new files and update binary files

--- Models/Message.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Treks.Models
-{
-    public class Message
-    {
-        public string ToUserId { get; set; }
-        public string FromUserId { get; set; }
-        public string MessageText { get; set; }
-    }
-}
