323b9d579cfcf86eee61f7f98887cc47e9b33f8c
Grant Watson <grant@gwsapp.net>
2025-12-27 11:54:59 -0500

    User Role Policy Update

--- obj/Debug/net8.0/Treks.AssemblyInfo.cs
+++ obj/Debug/net8.0/Treks.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Treks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4424315ae828722fa59543f123d691fbb75b3c06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08833796098ffe7037055039c31176cf6832422")]
 [assembly: System.Reflection.AssemblyProductAttribute("Treks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Treks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
